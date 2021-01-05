800fb52eca8f8e8f66cb902c2e953f0d3f29b04d
AnhelinaDiakova <Gelja4ka@gmail.com>
2021-01-05 20:59:42 +0200

    add transaction style

--- src/components/friends/FriendItem.jsx
+++ src/components/friends/FriendItem.jsx
@@ -5,8 +5,8 @@ export default function Friend({ avatar, name, isOnline }) {
   return (
     <li className={s.item}>
       <span className={isOnline ? s.isOnline : s.isOffline}></span>
-      <img className="avatar" src={avatar} alt={name} width="48" />
-      <p className="name">{name}</p>
+      <img className={s.avatar} src={avatar} alt={name} width="48" />
+      <p className={s.name}>{name}</p>
     </li>
   );
 }
--- src/components/transactions/TransactionHistory.jsx
+++ src/components/transactions/TransactionHistory.jsx
@@ -1,33 +1,37 @@
 import PropTypes from 'prop-types';
-import Transaction from './Transaction'
+import Transaction from './Transaction';
+import s from '../Transactions/Transactions.module.css';
 
 export default function TransactionHistory({ items }) {
-  return <table className="transaction-history">
-  <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
-    </tr>
-  </thead>
-   
-    <tbody>
-        {items.length > 0 && 
-          items.map(item => 
+  return (
+    <table className={s.transactionHistory}>
+      <thead className={s.head}>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.length > 0 &&
+          items.map(item => (
             <Transaction
               key={item.id}
               type={item.type}
               amount={item.amount}
               currency={item.currency}
-            />)
-        }
-  </tbody>
-</table>
+            />
+          ))}
+      </tbody>
+    </table>
+  );
 }
 
 TransactionHistory.propTypes = {
   items: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
-    }))
-}
+    }),
+  ),
+};
