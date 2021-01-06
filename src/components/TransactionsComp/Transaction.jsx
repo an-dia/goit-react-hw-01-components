df8c9b9ab8336af8b8eb49b83dd15df94fedd26c
AnhelinaDiakova <Gelja4ka@gmail.com>
2021-01-06 21:14:00 +0200

    edit name  folder

--- src/components/TransactionsComp/Transaction.jsx
+++ src/components/TransactionsComp/Transaction.jsx
@@ -1,16 +1,17 @@
 import PropTypes from 'prop-types';
 
-export default function Transaction({type, amount, currency= 'UAH'}) {
-  return <tr>
+export default function Transaction({ type, amount, currency = 'UAH' }) {
+  return (
+    <tr>
       <td>{type}</td>
       <td>{amount}</td>
       <td>{currency}</td>
     </tr>
+  );
 }
 
 Transaction.propTypes = {
   type: PropTypes.string.isRequired,
   amount: PropTypes.string.isRequired,
   currency: PropTypes.string.isRequired,
-}
-
+};
